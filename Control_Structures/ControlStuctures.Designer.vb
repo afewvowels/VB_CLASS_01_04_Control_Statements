1bff75b18e318f6a085a4729b3f9d0ab001dba86
afewvowels <keithbsmith87@gmail.com>
2018-09-17 14:34:19 -0400

    end of class

--- Control_Structures/ControlStuctures.Designer.vb
+++ Control_Structures/ControlStuctures.Designer.vb
@@ -31,6 +31,23 @@ Partial Class ControlStructures
         Me.OutputLabel3 = New System.Windows.Forms.Label()
         Me.ForNextLabel = New System.Windows.Forms.Label()
         Me.OutputLabel4 = New System.Windows.Forms.Label()
+        Me.ContinueForNextLabel = New System.Windows.Forms.Label()
+        Me.OutputLabel6 = New System.Windows.Forms.Label()
+        Me.ExitForLabel = New System.Windows.Forms.Label()
+        Me.OutputLabel5 = New System.Windows.Forms.Label()
+        Me.PatientLabel = New System.Windows.Forms.Label()
+        Me.PatientNameTextBox = New System.Windows.Forms.TextBox()
+        Me.DentalCalculatorGroupBox = New System.Windows.Forms.GroupBox()
+        Me.CleaningCheckBox = New System.Windows.Forms.CheckBox()
+        Me.FillingCheckBox = New System.Windows.Forms.CheckBox()
+        Me.XRayCheckBox = New System.Windows.Forms.CheckBox()
+        Me.CleaningLabel = New System.Windows.Forms.Label()
+        Me.FillingLabel = New System.Windows.Forms.Label()
+        Me.XRayLabel = New System.Windows.Forms.Label()
+        Me.DentalButton = New System.Windows.Forms.Button()
+        Me.TotalLabel = New System.Windows.Forms.Label()
+        Me.TotalTextBox = New System.Windows.Forms.TextBox()
+        Me.DentalCalculatorGroupBox.SuspendLayout()
         Me.SuspendLayout()
         '
         'IncrementButton
@@ -114,11 +131,171 @@ Partial Class ControlStructures
         Me.OutputLabel4.Size = New System.Drawing.Size(2, 15)
         Me.OutputLabel4.TabIndex = 8
         '
+        'ContinueForNextLabel
+        '
+        Me.ContinueForNextLabel.AutoSize = True
+        Me.ContinueForNextLabel.Location = New System.Drawing.Point(137, 170)
+        Me.ContinueForNextLabel.Name = "ContinueForNextLabel"
+        Me.ContinueForNextLabel.Size = New System.Drawing.Size(92, 13)
+        Me.ContinueForNextLabel.TabIndex = 13
+        Me.ContinueForNextLabel.Text = "Continue For Next"
+        '
+        'OutputLabel6
+        '
+        Me.OutputLabel6.AutoSize = True
+        Me.OutputLabel6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.OutputLabel6.Location = New System.Drawing.Point(137, 197)
+        Me.OutputLabel6.Name = "OutputLabel6"
+        Me.OutputLabel6.Size = New System.Drawing.Size(2, 15)
+        Me.OutputLabel6.TabIndex = 12
+        '
+        'ExitForLabel
+        '
+        Me.ExitForLabel.AutoSize = True
+        Me.ExitForLabel.Location = New System.Drawing.Point(12, 170)
+        Me.ExitForLabel.Name = "ExitForLabel"
+        Me.ExitForLabel.Size = New System.Drawing.Size(67, 13)
+        Me.ExitForLabel.TabIndex = 11
+        Me.ExitForLabel.Text = "Exit For Next"
+        '
+        'OutputLabel5
+        '
+        Me.OutputLabel5.AutoSize = True
+        Me.OutputLabel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.OutputLabel5.Location = New System.Drawing.Point(12, 197)
+        Me.OutputLabel5.Name = "OutputLabel5"
+        Me.OutputLabel5.Size = New System.Drawing.Size(2, 15)
+        Me.OutputLabel5.TabIndex = 10
+        '
+        'PatientLabel
+        '
+        Me.PatientLabel.AutoSize = True
+        Me.PatientLabel.Location = New System.Drawing.Point(6, 27)
+        Me.PatientLabel.Name = "PatientLabel"
+        Me.PatientLabel.Size = New System.Drawing.Size(43, 13)
+        Me.PatientLabel.TabIndex = 14
+        Me.PatientLabel.Text = "Patient:"
+        '
+        'PatientNameTextBox
+        '
+        Me.PatientNameTextBox.Location = New System.Drawing.Point(55, 27)
+        Me.PatientNameTextBox.Name = "PatientNameTextBox"
+        Me.PatientNameTextBox.Size = New System.Drawing.Size(115, 20)
+        Me.PatientNameTextBox.TabIndex = 15
+        '
+        'DentalCalculatorGroupBox
+        '
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.TotalTextBox)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.TotalLabel)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.DentalButton)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.XRayLabel)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.FillingLabel)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.CleaningLabel)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.XRayCheckBox)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.FillingCheckBox)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.CleaningCheckBox)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.PatientLabel)
+        Me.DentalCalculatorGroupBox.Controls.Add(Me.PatientNameTextBox)
+        Me.DentalCalculatorGroupBox.Location = New System.Drawing.Point(12, 234)
+        Me.DentalCalculatorGroupBox.Name = "DentalCalculatorGroupBox"
+        Me.DentalCalculatorGroupBox.Size = New System.Drawing.Size(230, 169)
+        Me.DentalCalculatorGroupBox.TabIndex = 16
+        Me.DentalCalculatorGroupBox.TabStop = False
+        Me.DentalCalculatorGroupBox.Text = "Dental Calculator"
+        '
+        'CleaningCheckBox
+        '
+        Me.CleaningCheckBox.AutoSize = True
+        Me.CleaningCheckBox.Location = New System.Drawing.Point(7, 58)
+        Me.CleaningCheckBox.Name = "CleaningCheckBox"
+        Me.CleaningCheckBox.Size = New System.Drawing.Size(67, 17)
+        Me.CleaningCheckBox.TabIndex = 16
+        Me.CleaningCheckBox.Text = "Cleaning"
+        Me.CleaningCheckBox.UseVisualStyleBackColor = True
+        '
+        'FillingCheckBox
+        '
+        Me.FillingCheckBox.AutoSize = True
+        Me.FillingCheckBox.Location = New System.Drawing.Point(7, 81)
+        Me.FillingCheckBox.Name = "FillingCheckBox"
+        Me.FillingCheckBox.Size = New System.Drawing.Size(52, 17)
+        Me.FillingCheckBox.TabIndex = 17
+        Me.FillingCheckBox.Text = "Filling"
+        Me.FillingCheckBox.UseVisualStyleBackColor = True
+        '
+        'XRayCheckBox
+        '
+        Me.XRayCheckBox.AutoSize = True
+        Me.XRayCheckBox.Location = New System.Drawing.Point(7, 104)
+        Me.XRayCheckBox.Name = "XRayCheckBox"
+        Me.XRayCheckBox.Size = New System.Drawing.Size(55, 17)
+        Me.XRayCheckBox.TabIndex = 18
+        Me.XRayCheckBox.Text = "X-Ray"
+        Me.XRayCheckBox.UseVisualStyleBackColor = True
+        '
+        'CleaningLabel
+        '
+        Me.CleaningLabel.AutoSize = True
+        Me.CleaningLabel.Location = New System.Drawing.Point(95, 58)
+        Me.CleaningLabel.Name = "CleaningLabel"
+        Me.CleaningLabel.Size = New System.Drawing.Size(46, 13)
+        Me.CleaningLabel.TabIndex = 19
+        Me.CleaningLabel.Text = "$135.00"
+        '
+        'FillingLabel
+        '
+        Me.FillingLabel.AutoSize = True
+        Me.FillingLabel.Location = New System.Drawing.Point(94, 81)
+        Me.FillingLabel.Name = "FillingLabel"
+        Me.FillingLabel.Size = New System.Drawing.Size(46, 13)
+        Me.FillingLabel.TabIndex = 20
+        Me.FillingLabel.Text = "$195.00"
+        '
+        'XRayLabel
+        '
+        Me.XRayLabel.AutoSize = True
+        Me.XRayLabel.Location = New System.Drawing.Point(94, 104)
+        Me.XRayLabel.Name = "XRayLabel"
+        Me.XRayLabel.Size = New System.Drawing.Size(46, 13)
+        Me.XRayLabel.TabIndex = 21
+        Me.XRayLabel.Text = "$150.00"
+        '
+        'DentalButton
+        '
+        Me.DentalButton.Location = New System.Drawing.Point(7, 130)
+        Me.DentalButton.Name = "DentalButton"
+        Me.DentalButton.Size = New System.Drawing.Size(75, 23)
+        Me.DentalButton.TabIndex = 22
+        Me.DentalButton.Text = "Calculate"
+        Me.DentalButton.UseVisualStyleBackColor = True
+        '
+        'TotalLabel
+        '
+        Me.TotalLabel.AutoSize = True
+        Me.TotalLabel.Location = New System.Drawing.Point(89, 139)
+        Me.TotalLabel.Name = "TotalLabel"
+        Me.TotalLabel.Size = New System.Drawing.Size(34, 13)
+        Me.TotalLabel.TabIndex = 23
+        Me.TotalLabel.Text = "Total:"
+        '
+        'TotalTextBox
+        '
+        Me.TotalTextBox.Location = New System.Drawing.Point(124, 136)
+        Me.TotalTextBox.Name = "TotalTextBox"
+        Me.TotalTextBox.ReadOnly = True
+        Me.TotalTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.TotalTextBox.TabIndex = 24
+        '
         'ControlStructures
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(352, 432)
+        Me.Controls.Add(Me.DentalCalculatorGroupBox)
+        Me.Controls.Add(Me.ContinueForNextLabel)
+        Me.Controls.Add(Me.OutputLabel6)
+        Me.Controls.Add(Me.ExitForLabel)
+        Me.Controls.Add(Me.OutputLabel5)
         Me.Controls.Add(Me.ForNextLabel)
         Me.Controls.Add(Me.OutputLabel4)
         Me.Controls.Add(Me.DoLoopWhileLabel)
@@ -130,6 +307,8 @@ Partial Class ControlStructures
         Me.Controls.Add(Me.IncrementButton)
         Me.Name = "ControlStructures"
         Me.Text = "Control Structures"
+        Me.DentalCalculatorGroupBox.ResumeLayout(False)
+        Me.DentalCalculatorGroupBox.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -143,5 +322,21 @@ Partial Class ControlStructures
     Friend WithEvents OutputLabel3 As System.Windows.Forms.Label
     Friend WithEvents ForNextLabel As System.Windows.Forms.Label
     Friend WithEvents OutputLabel4 As System.Windows.Forms.Label
+    Friend WithEvents ContinueForNextLabel As System.Windows.Forms.Label
+    Friend WithEvents OutputLabel6 As System.Windows.Forms.Label
+    Friend WithEvents ExitForLabel As System.Windows.Forms.Label
+    Friend WithEvents OutputLabel5 As System.Windows.Forms.Label
+    Friend WithEvents PatientLabel As System.Windows.Forms.Label
+    Friend WithEvents PatientNameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents DentalCalculatorGroupBox As System.Windows.Forms.GroupBox
+    Friend WithEvents DentalButton As System.Windows.Forms.Button
+    Friend WithEvents XRayLabel As System.Windows.Forms.Label
+    Friend WithEvents FillingLabel As System.Windows.Forms.Label
+    Friend WithEvents CleaningLabel As System.Windows.Forms.Label
+    Friend WithEvents XRayCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents FillingCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents CleaningCheckBox As System.Windows.Forms.CheckBox
+    Friend WithEvents TotalTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents TotalLabel As System.Windows.Forms.Label
 
 End Class
--- Control_Structures/ControlStuctures.vb
+++ Control_Structures/ControlStuctures.vb
@@ -3,6 +3,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles IncrementButton.Click
         ' Define variables
         Dim CounterInteger As Integer
+        Dim TestString As String
 
         ' Initialize counter variable
         CounterInteger = 1
@@ -57,6 +58,109 @@
                 OutputLabel4.Text &= " "
             End If
         Next CounterInteger
+
+        ' For Next loop example
+        For CounterInteger = 1 To 10 Step 1
+            ' Add counter value to label
+            OutputLabel5.Text &= CounterInteger.ToString
+            ' Test if counter is at last value, if not, append a space
+            ' to the label value
+            If CounterInteger <> 10 Then
+                OutputLabel5.Text &= " "
+            End If
+
+            If CounterInteger = 3 Then
+                Exit For
+            End If
+        Next CounterInteger
+
+        ' For Next loop example
+        For CounterInteger = 1 To 10 Step 1
+            ' Add counter value to label
+            OutputLabel6.Text &= CounterInteger.ToString
+
+            If CounterInteger = 3 Then
+                Continue For
+            End If
+
+            ' Test if counter is at last value, if not, append a space
+            ' to the label value
+            If CounterInteger <> 10 Then
+                OutputLabel6.Text &= " "
+            End If
+        Next CounterInteger
+
+        TestString = "A"
+
+        Select Case TestString
+            Case "A"
+                MessageBox.Show("Super")
+            Case "B"
+                MessageBox.Show("Good")
+            Case "C"
+                MessageBox.Show("Average")
+            Case "D", "F"
+                MessageBox.Show("Better luck next time")
+            Case Else
+                MessageBox.Show("Error determining grade")
+        End Select
+
+    End Sub
+
+    Private Sub DentalButton_Click(sender As Object, e As EventArgs) Handles DentalButton.Click
+        ' Calculate the bill
+
+        ' Initialize variables
+        Dim TempCounterInteger As Integer
+
+        Dim CLEANING_COST_INTEGER As Integer = 135
+        Dim FILLING_COST_INTEGER As Integer = 195
+        Dim XRAY_COST_INTEGER As Integer = 150
+
+        ' Check if name was entered
+        ' #1. Inequality test
+        If PatientNameTextBox.Text <> String.Empty Then
+            If (CleaningCheckBox.Checked = True) OrElse
+                (FillingCheckBox.Checked = True) OrElse
+                (XRayCheckBox.Checked = True) Then
+
+                If (CleaningCheckBox.Checked = True) Then
+                    TempCounterInteger += CLEANING_COST_INTEGER
+                End If
+                If (FillingCheckBox.Checked = True) Then
+                    TempCounterInteger += FILLING_COST_INTEGER
+                End If
+                If (XRayCheckBox.Checked = True) Then
+                    TempCounterInteger += XRAY_COST_INTEGER
+                End If
+
+                TotalTextBox.Text = TempCounterInteger.ToString("c")
+            Else
+                MessageBox.Show("Please make at least one selection",
+                                "Proceduce selection error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Exclamation)
+            End If
+        Else
+            MessageBox.Show("Please enter your name",
+                            "Name entry error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation)
+        End If
+
+        ' #2. Equality test
+        'If PatientNameTextBox.Text = String.Empty Then
+        '   Display warning
+        'Else
+        '   Proceed
+        'End If
+
+        ' #3. Negation test
+        'If Not (PatientNameTextBox.Text = String.Empty) Then
+        '   Proceed
+        'Else
+        '   Display warning
+        'End If
 
     End Sub
 End Class
